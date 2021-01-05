900c2d2fa2835b88649214a25379652e67a74443
tnmiller <71043718+tnmiller@users.noreply.github.com>
2021-01-05 14:58:14 -0500

    .java and .xml files from the app

---
+++ Day15/Implicit Intents App/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.implicitintents;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnMap, btnWeb, btnCall, btnCallFriend;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnMap = findViewById(R.id.btnMap);
+        btnWeb = findViewById(R.id.btnWeb);
+        btnCall = findViewById(R.id.btnCall);
+        btnCallFriend = findViewById(R.id.btnCallFriend);
+
+        btnCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                startActivity(intent);
+            }
+        });
+
+        btnCallFriend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:7034479564"));
+                startActivity(intent);
+            }
+        });
+
+        btnMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0?q=4100 Vacation Ln, Arlington, VA"));
+                startActivity(intent);
+            }
+        });
+
+        btnWeb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.backendless.com"));
+                startActivity(intent);
+            }
+        });
+    }
+}
